6346443722d87e1647530971cae768d3df04ceeb
B Brakel <48789339+bbrakel@users.noreply.github.com>
2019-11-03 11:36:38 +0100

    style: beautify

--- Front End Developer/Projects/Landing Page/js/app.js
+++ Front End Developer/Projects/Landing Page/js/app.js
@@ -11,12 +11,12 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 
 const navbar = document.querySelector('#navbar__list');
 const sections = document.querySelectorAll('section');
@@ -27,25 +27,26 @@ const header = document.querySelector('.page__header');
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
-// show or hide the navigation
-header.firstElementChild.addEventListener('click', () => {
-    navbar.classList.toggle('hidden');
-});
+// Set sections as active
+const setActive = target => {
+    document.querySelector('.your-active-class').classList.remove('your-active-class');
+    target.classList.add('your-active-class');
+};
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
 const navBuilder = () => {
     for (let section of sections) {
         let temp = `<li class="menu__link">${section.querySelector('h2').innerText}</li>`;
         navbar.innerHTML += temp;
-        };
+    };
     navbar.classList.toggle('hidden');
 };
 
@@ -54,32 +55,27 @@ document.addEventListener('scroll', e => {
     for (const key in sections) {
         if (window.pageYOffset >= (sections[key].offsetTop - 400)) {
             setActive(sections[key]);
-        }}});
+        }
+    }
+});
 
-
-// Scroll to anchor ID using scrollTO event
-
+// Build menu 
+navBuilder();
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
-
-// Build menu 
-navBuilder();
+ */
 
 // Scroll to section on link click
-
 navbar.addEventListener('click', e => {
     let targetSect = document.querySelector(`[data-nav="${e.target.innerText}"]`);
     window.scrollTo(0, targetSect.offsetTop);
     navbar.classList.add('hidden');
 })
-    
 
-// Set sections as active
-const setActive = target => {
-    document.querySelector('.your-active-class').classList.remove('your-active-class');
-    target.classList.add('your-active-class');
-};
+// show or hide the navigation
+header.firstElementChild.addEventListener('click', () => {
+    navbar.classList.toggle('hidden');
+});
