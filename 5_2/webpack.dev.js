eb23cf2b2391b9a822327564ff5638b13576ccb5
B Brakel <48789339+bbrakel@users.noreply.github.com>
2019-12-09 23:32:24 +0100

    docs: changed dir names & new lesson

---
+++ 5_2/webpack.dev.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebPackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    module: {
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    },
+    plugins: [
+        new htmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new BundleAnalyzerPlugin()
+    ]
+}
