7794e126d79301db88f5cc6f2a3fb3f38969f01c
B Brakel <48789339+bbrakel@users.noreply.github.com>
2019-12-12 10:50:43 +0100

    docs: removed unnessary files/dirs

--- 5_2/src/client/index.js
+++ 5_2/src/client/index.js
@@ -1,10 +1,10 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
-import { stylesheetBase } from './styles/base.scss'
-import { stylesheetFooter } from './styles/footer.scss'
-import { stylesheetForm } from './styles/form.scss'
-import { stylesheetHeader } from './styles/header.scss'
-import { stylesheetResets } from './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
 
 export {
     checkForName,
@@ -13,13 +13,3 @@ export {
 
 console.log(checkForName);
 console.log(handleSubmit);
-console.log(stylesheetBase);
-console.log(stylesheetFooter);
-console.log(stylesheetForm);
-console.log(stylesheetHeader);
-console.log(stylesheetResets);
-
-
-
-
-alert("I EXIST")
