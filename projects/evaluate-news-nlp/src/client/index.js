efd7e2d3b7730084b8c5a3ff54eb86dde0c080d6
B Brakel <48789339+bbrakel@users.noreply.github.com>
2019-12-19 13:48:50 +0100

    chore: beautify

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,5 +1,9 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import {
+    checkForName
+} from './js/nameChecker'
+import {
+    handleSubmit
+} from './js/formHandler'
 
 import './styles/resets.scss'
 import './styles/base.scss'
