682126add3ecbb1c3ef4a3151c95269af6423a84
B Brakel <48789339+bbrakel@users.noreply.github.com>
2019-12-22 21:10:39 +0100

    help! D:

---
+++ projects/evaluate-news-nlp/__tests__/filterByTerm.spec.js
@@ -0,0 +1,6 @@
+import { handleSubmit } from "../src/client/js/formHandler"
+
+describe("Filter function", () => {
+  test("Testing the API call", () => {
+    handleSubmit()
+  })})
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const regeneratorRuntime = require("regenerator-runtime")
 
 const express = require('express');
 const bodyParser = require('body-parser');
