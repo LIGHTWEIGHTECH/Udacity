6dcc32ced6e6e9559418c82c6fb275433f744f55
B Brakel <48789339+bbrakel@users.noreply.github.com>
2019-12-25 16:34:19 +0100

    chore: packages

--- projects/fend-capstone-travel-app/src/server/server.js
+++ projects/fend-capstone-travel-app/src/server/server.js
@@ -2,41 +2,30 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+
+/* Dependencies */
+const bodyParser = require('body-parser')
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('../dist'));
 
 
 // Setup Server
-
-// Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
-
-// Start up an instance of app
-
-/* Dependencies */
-/* Middleware*/
-
-//Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
-
-// Initialize the main project folder
-
-// Spin up the server
-// Callback to debug
-
-// Initialize all route with a callback function
-
-// Callback function to complete GET '/all'
-
-// Post Route
-  
+const server = app.listen(port, () => {
+    console.log(`running on localhost: ${port}`)
+})
