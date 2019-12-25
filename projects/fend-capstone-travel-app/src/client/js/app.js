413f1c433e921f32b4ca23f5ffc7294f8a854865
B Brakel <48789339+bbrakel@users.noreply.github.com>
2019-12-25 16:00:48 +0100

    chore: file hierarchy

--- projects/fend-capstone-travel-app/src/client/js/app.js
+++ projects/fend-capstone-travel-app/src/client/js/app.js
@@ -1,3 +1,9 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
 // Personal API Key for OpenWeatherMap API
 
 // Event listener to add function to existing HTML DOM element
